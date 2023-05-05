e94ff9ee08a6c39559dee269163cf5eee161a4ea
Marijn <marijnvanloo@gmail.com>
2023-05-05 17:25:20 +0200

    rhf

--- src/pages/copy-creator.tsx
+++ src/pages/copy-creator.tsx
@@ -1,117 +1,69 @@
-import { BlitzPage } from "@blitzjs/next"
 import { useMutation } from "@blitzjs/rpc"
-import { Button, MultiSelect, Select, Stack, Text, Textarea, Title } from "@mantine/core"
+import { zodResolver } from "@hookform/resolvers/zod"
+import {
+  Button,
+  Card,
+  MultiSelect,
+  Select,
+  SelectItem,
+  Stack,
+  Text,
+  Textarea,
+  Title,
+  createStyles,
+} from "@mantine/core"
 import { useState } from "react"
-import { DEFAULT_PROMPT } from "src/core/components/copy-creator/constants"
-import CopyCreatorMuation from "src/core/components/copy-creator/mutations/send-prompt"
+import { Controller, useForm } from "react-hook-form"
+import { TARGET_AUDIENCES, TONE_OF_VOICE } from "src/core/copy-creator/constants"
+import CopyCreatorMuation from "src/core/copy-creator/mutations/send-prompt"
+import { CopyCreatorInput } from "src/core/copy-creator/zod"
 import Layout from "src/core/layouts/Layout"
 
 function useCopyCreatorMutation() {
   return useMutation(CopyCreatorMuation)
 }
 
-const toneOfVoice = [
-  "Formal",
-  "Informal",
-  "Approachable",
-  "Professional",
-  "Conversational",
-  "Friendly",
-  "Funny",
-  "Confident",
-  "Inspiring",
-  "Engaging",
-  "Witty",
-  "Humorous",
-  "Approval-seeking",
-  "Thoughtful",
-  "Supportive",
-  "Encouraging",
-  "Informative",
-  "Assertive",
-  "Empathetic",
-  "Authentic",
-  "Innovative",
-  "Optimistic",
-  "Playful",
-  "Sarcastic",
-  "Sincere",
-  "Serious",
-  "Silly",
-  "Expert",
-]
-
-const targetAudiences = [
-  "Millennials",
-  "Generation Z",
-  "Baby Boomers",
-  "Generation X",
-  "High-Income Consumers",
-  "Low-Income Consumers",
-  "Urban Consumers",
-  "Rural Consumers",
-  "Suburban Consumers",
-  "Male Consumers",
-  "Female Consumers",
-]
-
-const lengtSelectData = [
+const lengtSelectData: Array<SelectItem> = [
   { label: "Short", value: "0 - 20" },
   { label: "Medium", value: "20 - 100" },
   { label: "Long", value: "100 - 500" },
 ]
 
-const toneOfVoiceSelectData = toneOfVoice.sort().map((item) => ({ label: item, value: item }))
-const targetAudiencesSelectData = targetAudiences
-  .sort()
-  .map((item) => ({ label: item, value: item }))
+const toneOfVoiceSelectData: Array<SelectItem> = TONE_OF_VOICE.sort().map((item) => ({
+  label: item,
+  value: item,
+}))
+const targetAudiencesSelectData: Array<SelectItem> = TARGET_AUDIENCES.sort().map((item) => ({
+  label: item,
+  value: item,
+}))
 
-const CopyCreator: BlitzPage = () => {
-  const [toneOfVoice, setToneOfVoice] = useState([])
-  const [targetAudiences, setTargetAudiences] = useState([])
-  const [cta, setCta] = useState("")
-  const [length, setLength] = useState<string | null>(null)
+function CopyCreator(): JSX.Element {
   const [sendPrompt, { isLoading }] = useCopyCreatorMutation()
-  const [result, setResult] = useState("")
+  const [result, setResult] = useState<string>("")
+  const { classes } = useStyles(undefined, { name: CopyCreator.name })
+  const copyCreatorForm = useForm({
+    defaultValues: {
+      toneOfVoice: [],
+      targetAudiences: [],
+      callToAction: "",
+      copyLength: "",
+    },
+    resolver: zodResolver(CopyCreatorInput),
+  })
 
-  const finalPrompt =
-    DEFAULT_PROMPT +
-    "```" +
-    "Tone of voice: " +
-    toneOfVoice.join(", ") +
-    ".\n" +
-    "Target audience: " +
-    targetAudiences.join(", ") +
-    ".\n" +
-    "Length: " +
-    length +
-    " words" +
-    ".\n" +
-    "Call to action: " +
-    cta +
-    ".\n" +
-    "```"
-
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-
+  const handleSubmit = copyCreatorForm.handleSubmit(async (values) => {
     try {
-      await sendPrompt(
-        {
-          prompt: finalPrompt,
+      await sendPrompt(values, {
+        onSuccess(data) {
+          setResult(data)
         },
-        {
-          onSuccess(data) {
-            setResult(data)
-          },
-        }
-      )
+      })
     } catch (error) {
       console.log(error)
     }
-  }
+  })
 
-  console.log({ finalPrompt })
   console.log({ result })
 
   return (
@@ -129,49 +81,85 @@ const CopyCreator: BlitzPage = () => {
 
       <Stack align="center">
         <form onSubmit={handleSubmit}>
-          <Stack align="center" mt="xl" spacing="md" style={{ width: 300 }}>
-            <MultiSelect
-              data={toneOfVoiceSelectData}
-              value={toneOfVoice}
-              onChange={setToneOfVoice}
-              label="Select keywords for your tone of voice"
-              style={{ width: "100%" }}
-            />
+          <Card shadow="sm" style={{ width: 300 }}>
+            <Stack align="center" mt="xl" spacing="md">
+              <Controller
+                control={copyCreatorForm.control}
+                name="toneOfVoice"
+                render={({ field, fieldState }) => (
+                  <MultiSelect
+                    {...field}
+                    data={toneOfVoiceSelectData}
+                    label="Select keywords for your tone of voice"
+                    searchable
+                    nothingFound="Nothing found"
+                    classNames={{ root: classes.root }}
+                    error={fieldState.error && <span>{fieldState.error.message}</span>}
+                  />
+                )}
+              />
 
-            <MultiSelect
-              data={targetAudiencesSelectData}
-              label="Select your target audience"
-              value={targetAudiences}
-              onChange={setTargetAudiences}
-              style={{ width: "100%" }}
-            />
+              <Controller
+                control={copyCreatorForm.control}
+                name="targetAudiences"
+                render={({ field, fieldState }) => (
+                  <MultiSelect
+                    {...field}
+                    data={targetAudiencesSelectData}
+                    label="Select your target audience"
+                    searchable
+                    nothingFound="Nothing found"
+                    classNames={{ root: classes.root }}
+                    error={fieldState.error && <span>{fieldState.error.message}</span>}
+                  />
+                )}
+              />
 
-            <Select
-              data={lengtSelectData}
-              label="Length of copy"
-              value={length}
-              onChange={setLength}
-              style={{ width: "100%" }}
-            />
+              <Controller
+                control={copyCreatorForm.control}
+                name="copyLength"
+                render={({ field, fieldState }) => (
+                  <Select
+                    {...field}
+                    data={lengtSelectData}
+                    label="Length of copy"
+                    classNames={{ root: classes.root }}
+                    error={fieldState.error && <span>{fieldState.error.message}</span>}
+                  />
+                )}
+              />
 
-            <Textarea
-              label="Call to action"
-              value={cta}
-              onChange={(e) => setCta(e.currentTarget.value)}
-              placeholder="What do you want your audience to do?"
-              minRows={4}
-              style={{ width: "100%" }}
-            />
-            <Button mt="md" type="submit" loading={isLoading}>
-              ✨ Create copy ✨
-            </Button>
+              <Controller
+                control={copyCreatorForm.control}
+                name="callToAction"
+                render={({ field, fieldState }) => (
+                  <Textarea
+                    {...field}
+                    label="Call to action"
+                    placeholder="What do you want your audience to do?"
+                    minRows={4}
+                    classNames={{ root: classes.root }}
+                    error={fieldState.error && <span>{fieldState.error.message}</span>}
+                  />
+                )}
+              />
 
-            {result && <Text>{result}</Text>}
-          </Stack>
+              <Button mt="md" type="submit" loading={isLoading} disabled={isLoading}>
+                ✨ Create copy ✨
+              </Button>
+            </Stack>
+          </Card>
         </form>
+        <Card shadow="sm" style={{ width: 300, minHeight: 300 }}>
+          <Text>{result}</Text>
+        </Card>
       </Stack>
     </Layout>
   )
 }
+
+const useStyles = createStyles((theme) => ({
+  root: { width: "100%" },
+}))
 
 export default CopyCreator
