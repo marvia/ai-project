aa175e91c754d7fd6bf9aa34f499ed791d8008a3
us <usama.farag@getmarvia.com>
2023-05-24 10:51:30 +0200

    internal api

---
+++ src/pages/api/chat.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from "next"
+import openai from "../../core/utils/openai"
+
+type Data = {
+  name: string
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const { prompt } = req.body
+  const messages = [{ role: "user", content: prompt }]
+  const completion = await openai
+    .createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: messages,
+      temperature: 0,
+      max_tokens: 1000,
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  res.send(completion.data.choices[0].message.content)
+  // receive some user data, ask chat gpt to generate something and return to the user
+}
